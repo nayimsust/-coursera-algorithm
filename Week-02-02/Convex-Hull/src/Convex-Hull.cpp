f9dacee63d37ec490ded8c256541d33227d2e792
nayimsust <nayimsust@gmail.com>
2016-03-21 20:41:57 +0800

    Convex Hull source staring update

---
+++ Week-02-02/Convex-Hull/src/Convex-Hull.cpp
@@ -0,0 +1,39 @@
+//============================================================================
+// Name        : Convex-Hull.cpp
+// Author      : nayimsust
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+
+/* farthest pair of points is the extreme point in convex hull
+ *
+ */
+
+
+/*
+ * polar coordinate system (r, theta)
+ * theta is angle with x axis counter clock wise and r is the distance.
+ *
+ * cartesian coordinate system(x, y)
+ *
+ */
+
+
+/*
+ * Graham scan algorithm to solve the convex hull problem
+ * steps.
+ *
+ * 1.	find the minimum point P in basis of minimum y axis value.
+ * 2. 	sort the points with the polar angle with respect to P point.
+ * 3.
+ */
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
