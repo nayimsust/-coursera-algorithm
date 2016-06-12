827d0cf1e800857637bfde5ffa729e11b57f95a4
Nazmul Alam <nayimsust@gmail.com>
2016-06-12 14:39:55 +0800

    Started Binary Search Tree implementation.

---
+++ Week-04-02/BST/src/BinarySearchTree.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : BinarySearchTree.cpp
+// Author      : nayimsust@gmail.com
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
